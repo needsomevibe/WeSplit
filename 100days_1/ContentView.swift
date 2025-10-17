4ac93833200c5341c0cfafa8590be3b0119cc59e
Marsel Khasanov <miorauz@gmail.com>
2025-10-17 14:11:20 +0200

    Initial Commit

---
+++ 100days_1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  100days_1
+//
+//  Created by needsomevibe on 17/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 100days_1/_00days_1App.swift
@@ -0,0 +1,17 @@
+//
+//  _00days_1App.swift
+//  100days_1
+//
+//  Created by needsomevibe on 17/10/25.
+//
+
+import SwiftUI
+
+@main
+struct _00days_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
